ffc49b1280b2d746cae293f342573ac45c82453b
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-15 10:34:51 -0500

    replacing string content

--- app.js
+++ app.js
@@ -12,3 +12,24 @@ function myFunction() {
   var pos = str.indexOf("locate");
   document.getElementById("finding").innerHTML = pos;
 }
+
+function myFunction1() {
+  var str = document.getElementById("p2").innerHTML;
+  var pos = str.lastIndexOf("locate");
+  document.getElementById("last").innerHTML = pos;
+}
+
+/****************************************************
+*                 The slice() Method                *
+****************************************************/
+var str = "Apple, Banana, Kiwi";
+document.getElementById("slice").innerHTML = str.slice(7,13);
+
+/****************************************************
+*              Replacing String Content             *
+****************************************************/
+function myFunction() {
+  var str = document.getElementById("replace").innerHTML;
+  var txt = str.replace("Microsoft","W3Schools");
+  document.getElementById("replace").innerHTML = txt;
+}
